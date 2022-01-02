9881e933c5eb0f9d364307098d8bd8a171ef6112
ilirianthaci18 <ilirianthaqi@live.com>
2022-01-02 16:57:55 +0100

    project init

---
+++ educatex-lms-svc/src/main/java/com/educatex/lms/EducatexLmsSvcApplication.java
@@ -0,0 +1,13 @@
+package com.educatex.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducatexLmsSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducatexLmsSvcApplication.class, args);
+    }
+
+}
